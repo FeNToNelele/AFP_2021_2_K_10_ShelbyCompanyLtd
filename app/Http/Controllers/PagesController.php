18c2119fa1b57aab03960c83b69225d4a134e443
KecseKaroly <83023819+KecseKaroly@users.noreply.github.com>
2022-03-12 18:47:11 +0100

    Mai eseményeket lekérdező function megírása a PagesControllerben

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -1,1 +1,18 @@
+<?php
 
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PagesController extends Controller
+{
+    public function index() {
+
+        $maiEsemenyek = DB::table('esemeny')
+        ->select('id','megnevezes','kapacitas','leiras','kezdet','veg','helyszin','dolgozoid')
+        ->whereDate('kezdet', '=', date('Y.m.d'))
+        ->get();
+        return view('index')->with('maiEsemenyek', $maiEsemenyek);
+    }
+}
