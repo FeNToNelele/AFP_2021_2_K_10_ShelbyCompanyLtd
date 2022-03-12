2b64d6db5eb4041ba8aba0b2c78b9abe1d064c71
FeNToNelele <sarosigab@gmail.com>
2022-03-12 11:23:40 +0100

    Események menüpont hozzáadása

---
+++ resources/views/events.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@yield('navbar')
+@section('title', 'Események')
+
+@section('content')
+<div class="bg-white rounded-xl shadow-lg mx-5 lg:mx-52 py-10 mt-10">
+    <p class="font-light text-center text-3xl lg:text-5xl ">Eseményeink:</p>
+</div>
+
+@endsection
