2da892309ec9be8ffaf878a4613879874c221f9a
FeNToNelele <sarosigab@gmail.com>
2022-03-13 16:08:06 +0100

    Eseményekhez tartozó controller refaktorálása
    A refaktorált mappaszerkezet igényelte a controller módosítását is.

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 class EventsController extends Controller
 {
     public function listAllEvents() {
-        return view('events');
+        return view('events.events');
+    }
+
+    public function listAnEvent() {
+        return view('events.event');
     }
 }
