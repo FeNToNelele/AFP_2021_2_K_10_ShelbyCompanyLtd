062ab3ee592d399ebe3524764c2a46a7a5b5ea5c
FeNToNelele <sarosigab@gmail.com>
2022-03-13 16:10:13 +0100

    Bejelentkezés - Emlékezz rám gomb javítása
    Az emlékezz rám gomb kötelezőre volt rakva, pedig ez egy opcionális lehetőség.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,7 +18,7 @@
             <div class="flex items-start my-5">
                 <div class="flex items-start">
                     <div class="flex items-center h-5">
-                    <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required>
+                    <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800">
                     </div>
                     <div class="ml-3 text-sm">
                     <label for="remember" class="font-medium text-gray-100 dark:text-gray-300">Emlékezz rám</label>
