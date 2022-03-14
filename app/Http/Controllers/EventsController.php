8e1c2d149462406223491d4e1b57fc87dbd607c9
KecseKaroly <83023819+KecseKaroly@users.noreply.github.com>
2022-03-14 13:19:30 +0100

    Esemény részleteit lekérő function bővítése DB lekérdezésekkel

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -16,6 +16,18 @@ class EventsController extends Controller
     }
 
     public function listAnEvent() {
-        return view('events.event');
+        public function listAnEvent($id) {
+        $esemeny = DB::table('esemeny')
+        ->select('id','megnevezes','kapacitas','leiras','kezdet','veg','helyszin','dolgozoid')
+        ->where('id',$id)
+        ->first();
+        $jelentkezesek = DB::table('jelentkezes')
+        ->where('esemenyId',$id)
+        ->count('jelentkezesId');
+        $szervezo = DB::table('users')
+        ->select('name')
+        ->where('id',$esemeny->dolgozoid)->first();
+        return view('events.event')->with('esemeny', $esemeny)->with('jelentkezesek', $jelentkezesek)->with('szervezo',$szervezo);
+    }
     }
 }
