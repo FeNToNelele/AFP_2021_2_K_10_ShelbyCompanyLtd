31f60cfd2696eaaebcf28a18d5c55fed5cc2d2d9
KecseKaroly <83023819+KecseKaroly@users.noreply.github.com>
2022-03-14 13:19:08 +0100

    Minden eseményt kiirató controller function bővítése DB lekérdezéssel

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -7,7 +7,12 @@ use Illuminate\Http\Request;
 class EventsController extends Controller
 {
     public function listAllEvents() {
-        return view('events.events');
+       //Még nem lezajlott események kilistázása
+        $esemenyek = DB::table('esemeny')
+        ->select('id','megnevezes','kapacitas','leiras','kezdet','veg','helyszin','dolgozoid')
+        ->whereDate('kezdet', '>', date('Y.m.d'))
+        ->get();
+        return view('events.events')->with('esemenyek', $esemenyek);
     }
 
     public function listAnEvent() {
