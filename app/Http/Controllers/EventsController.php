c98c3ff280a7ccc3c67b13922f780d417de157d0
KecseKaroly <83023819+KecseKaroly@users.noreply.github.com>
2022-03-14 15:29:22 +0100

    Auth modul importálása az EventsController-be

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Support\Facades\Auth;
 class EventsController extends Controller
 {
     public function listAllEvents() {
