82460291bd031580e4118db0c636ffbfdd8be7d3
FeNToNelele <sarosigab@gmail.com>
2022-03-15 12:51:37 +0100

    Regisztráció hiba front-end javítás
    A select-ből hiányzott a name mező.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -91,7 +91,7 @@
             </div>
             <div class="my-5">
                 <label for="accountType" class="block mb-2 text-sm font-medium text-gray-100 dark:text-gray-300">Válasszon fióktípust:</label>
-                <select id="accountType" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500" onchange="showExtra();">
+                <select name="accountType" id="accountType" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500" onchange="showExtra();">
                   <option selected disabled hidden>Válasszon...</option>
                   <option value="guest">Külső vendég</option>
                   <option value="student">Hallgató</option>
