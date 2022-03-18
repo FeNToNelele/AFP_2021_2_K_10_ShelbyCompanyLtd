d6b4d6028eee0e206886a34a20d983b4e2a12b42
FeNToNelele <sarosigab@gmail.com>
2022-03-18 19:15:54 +0100

    Refaktorált bejelentkezés menü

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,34 +3,34 @@
 
 @section('content')
 <div class="flex justify-center items-center h-screen">
-    <div class="mt-10 p-10 w-96 bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <form class="space-y-6" method="POST" action="{{ route('login') }}">
+    <div class="p-5 w-96 bg-white rounded-lg border border-gray-200 shadow-md">
+        <form method="POST" action="{{ route('login') }}">
             @csrf
-            <h3 class="text-2xl text-center text-gray-900 dark:text-white my-5">Bejelentkezés</h3>
+            <h3 class="text-2xl text-center text-gray-900 mb-3">Bejelentkezés</h3>
             <div>
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-100 dark:text-gray-300">E-Mail cím:</label>
-                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-black" placeholder="name@company.com" required>
+                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">E-Mail cím:</label>
+                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full" placeholder="name@company.com" required>
             </div>
-            <div class="mt-5">
-                <label for="password" class="block mb-2 text-sm font-medium text-gray-100 dark:text-gray-300">Jelszó:</label>
-                <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-black" required>
+            <div class="mt-3">
+                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Jelszó:</label>
+                <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required>
             </div>
-            <div class="flex items-start my-5">
+            <div class="flex items-start my-3">
                 <div class="flex items-start">
                     <div class="flex items-center h-5">
-                    <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800">
+                    <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 bg-gray-50 rounded border border-gray-900 focus:ring-3 focus:ring-blue-300">
                     </div>
                     <div class="ml-3 text-sm">
-                    <label for="remember" class="font-medium text-gray-100 dark:text-gray-300">Emlékezz rám</label>
+                    <label for="remember" class="font-medium text-gray-900">Emlékezz rám</label>
                     </div>
                 </div>
             </div>
-            <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Bejelentkezés</button>
-            <div class="flex items-start my-5">
-                <a href="{{ route('password.request') }}" class="text-sm font-light text-right text-white dark:text-white"><u><i>Elfelejtette jelszavát?</i></u></a>
+            <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Bejelentkezés</button>
+            <div class="flex items-start my-3">
+                <a href="{{ route('password.request') }}" class="text-sm font-light text-right text-blue-700 hover:underline"><i>Elfelejtette jelszavát?</i></a>
             </div>
-            <div class="text-sm font-light text-white dark:text-white">
-                Nincs még fiókja? <a href="{{ route('register') }}" class="text-blue-700 hover:underline dark:text-blue-500"><i><u>Regisztráljon</u></i></a>
+            <div class="text-sm font-light text-gray-900">
+                Nincs még fiókja? <a href="{{ route('register') }}" class="text-blue-700 hover:underline"><i>Regisztráljon</i></a>
             </div>
         </form>
     </div>
