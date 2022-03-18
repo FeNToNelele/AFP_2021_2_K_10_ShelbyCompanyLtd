6e9e06bc6dff4f7398f791d4c5c94eb85a0fa42a
FeNToNelele <sarosigab@gmail.com>
2022-03-18 19:06:14 +0100

    Middleware védelem hozzáadása egyes routeokhoz
    Ha a felhasználó nincs bejelentkezve, akkor automatikus átirányítás történik a megjelölt oldalakon.

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,11 @@ Route::get('/', [App\Http\Controllers\PagesController::class, 'index'])->name('i
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/myProfile', [App\Http\Controllers\UserController::class, 'myProfile'])->name('myProfile');
-Route::post('/apply', [App\Http\Controllers\EventsController::class, 'apply'])->name('apply');
-Route::get('/appliedEvents', [App\Http\Controllers\EventsController::class, 'appliedEvents'])->name('appliedEvents');
-Route::delete('/abandon', [App\Http\Controllers\EventsController::class, 'abandonEvent']);
+Route::get('/myProfile', [App\Http\Controllers\UserController::class, 'myProfile'])->name('myProfile')->middleware('auth');
+Route::post('/apply', [App\Http\Controllers\EventsController::class, 'apply'])->name('apply')->middleware('auth');
+Route::get('/appliedEvents', [App\Http\Controllers\EventsController::class, 'appliedEvents'])->name('appliedEvents')->middleware('auth');
+Route::delete('/abandon', [App\Http\Controllers\EventsController::class, 'abandonEvent'])->middleware('auth');
 
 Route::get('/events', [EventsController::class, 'listAllEvents'])->name('events');
-Route::get('/events/{id}', [EventsController::class, 'listAnEvent']);
-Route::get('/createEvent', [EventsController::class, 'createEvent'])->name('createEvent');
+Route::get('/events/{id}', [EventsController::class, 'listAnEvent'])->middleware('auth');
+Route::get('/createEvent', [EventsController::class, 'createEvent'])->name('createEvent')->middleware('auth');
