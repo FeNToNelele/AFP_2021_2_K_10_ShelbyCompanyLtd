66057e499436ec7fdb24b00a395fc0e82f868a3c
KecseKaroly <83023819+KecseKaroly@users.noreply.github.com>
2022-03-19 17:02:58 +0100

    Hozzáférés korlátozása szerver oldalon is esemény hozzáadásakor

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -54,16 +54,20 @@ class EventsController extends Controller
     }
 
     public function registerEvent(Request $request) {
-        DB::table('esemeny')->insert([
-            'megnevezes' => $request->input('megnevezes'),
-            'kapacitas' => $request->input('kapacitas'),
-            'leiras' => $request->input('leiras'),
-            'kezdet' => date('Y-m-d h:i:s', strtotime($request->input('kezdet'))),
-            'veg' => date('Y-m-d h:i:s', strtotime($request->input('veg'))),
-            'helyszin' => $request->input('helyszin'),
-            'dolgozoId' => Auth::user()['id']  
-        ]);
-        echo "Sikeres esemény felvitel!";
+        if(Auth::user()['accountType'] == 'teacher') 
+        {
+            DB::table('esemeny')->insert([
+                'megnevezes' => $request->input('megnevezes'),
+                'kapacitas' => $request->input('kapacitas'),
+                'leiras' => $request->input('leiras'),
+                'kezdet' => date('Y-m-d h:i:s', strtotime($request->input('kezdet'))),
+                'veg' => date('Y-m-d h:i:s', strtotime($request->input('veg'))),
+                'helyszin' => $request->input('helyszin'),
+                'dolgozoId' => Auth::user()['id']  
+            ]);
+            echo "Sikeres esemény felvitel!";
+        }
+        else echo "Nincs hozzá jogosultsága!";
         return view('home');
     }
 
