329bff0c05c43a0f5de2d1960312a55122921531
FeNToNelele <sarosigab@gmail.com>
2022-04-17 16:30:09 +0200

    Saját eseményeim front-end design

--- resources/views/events/manageEvents.blade.php
+++ resources/views/events/manageEvents.blade.php
@@ -2,11 +2,18 @@
 @section('title', 'Kezelés')
 
 @section('content')
-<div class="flex h-screen">
-    <a href="{{ route('createEvent') }}" class="m-auto bg-green-600 rounded-xl shadow-lg font-iight text-2xl px-5 py-2.5 text-gray-100 transition text-center hover:bg-green-500 hover:text-gray-100">
-        Új esemény hozzáadása
-    </a>
+<div class="flex">
+    <div class="grid grid-rows-1 grid-cols-3 w-full my-5">
+        <div></div>
+        <div>
+            <a href="{{ route('createEvent') }}" class="m-auto bg-green-600 rounded-xl shadow-lg font-iight text-2xl px-5 py-2.5 text-gray-100 transition text-center hover:bg-green-500 hover:text-gray-100">
+                Új esemény hozzáadása
+            </a>
+        </div>
+        <div></div>
+    </div>
 </div>
+
 <div class="flex h-screen">
     <div class="mx-auto">
         @foreach($esemenyek as $esemeny)
@@ -14,19 +21,24 @@
             <a href="/events/{{ $esemeny->id }}">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{{ $esemeny->megnevezes }}</h5>
             </a>
-            <p class="mb-3 font-normal text-gray-500">{{ $esemeny->leiras }}</p>
-            <p class="mb-3 font-medium text-gray-500">Kezdet: {{ $esemeny->kezdet }}</p>
-            <p class="mb-3 font-medium text-gray-500">Vége: {{ $esemeny->veg }}</p>
-            <form method="POST" action="{{ route('editEventForm') }}" >
+            <p class="mb-3 font-normal text-gray-700">{{ $esemeny->leiras }}</p>
+            <p class="mb-3 font-medium text-gray-700">Kezdet: {{ $esemeny->kezdet }}</p>
+            <p class="mb-3 font-medium text-gray-700">Vége: {{ $esemeny->veg }}</p>
+            <form method="POST" action="{{ route('editEventForm') }}" class="mb-2">
                 @csrf
                 <input type="hidden" value="{{ $esemeny->id }}" name="esemenyId">
-                <input type="submit" value="Módosítás" class="m-auto bg-red-600 rounded-xl shadow-lg font-iight text-2xl px-5 py-2.5 text-gray-100 transition text-center hover:bg-red-500 hover:text-gray-100">
+                <button type="submit" class="m-auto bg-green-600 rounded-xl shadow-lg font-iight text-2xl px-5 py-2.5 text-gray-100 transition hover:bg-green-500 hover:text-black">QR-Kód generálása</button>
+            </form>
+            <form method="POST" action="{{ route('editEventForm') }}" class="mb-2">
+                    @csrf
+                <input type="hidden" value="{{ $esemeny->id }}" name="esemenyId">
+                <button type="submit" class="m-auto bg-yellow-400 rounded-xl shadow-lg font-iight text-2xl px-5 py-2.5 text-gray-100 transition hover:bg-yellow-300 hover:text-black">Módosítás</button>
             </form>
             <form action = "{{ route('deleteEvent') }}" method="POST">
                 @csrf
                 {{ method_field('delete') }}
                 <input type="hidden" name="esemenyId" value="{{ $esemeny->id }}">
-                <input type="submit" value="Törlés"  class="m-auto bg-red-600 rounded-xl shadow-lg font-iight text-2xl px-5 py-2.5 text-gray-100 transition text-center hover:bg-red-500 hover:text-gray-100"> 
+                <button type="submit" value="Törlés"  class="m-auto bg-red-900 rounded-xl shadow-lg font-iight text-2xl px-5 py-2.5 text-gray-100 transition hover:bg-red-700 hover:text-gray-100">Törlés</button>
             </form>
         </div>
         @endforeach
