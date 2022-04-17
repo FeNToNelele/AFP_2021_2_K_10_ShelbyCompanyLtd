65545babc31fa83d076c59b9ff9787aab5062fa1
KecseKaroly <83023819+KecseKaroly@users.noreply.github.com>
2022-04-17 15:36:38 +0200

    Frontenden a jelentkezés lekezelése

--- resources/views/events/verify.blade.php
+++ resources/views/events/verify.blade.php
@@ -4,11 +4,17 @@
 @section('content')
 <div class="flex h-screen">
     <div class="mx-auto">
-        <form action="" class="mt-5">
-            <input type="hidden" value="{{ $esemeny->id }}" name="esemenyId">
-            <input type="hidden" value="{{ $felhasznalo->id }}" name="felhasznaloId">
+        @if($applied != 0)
+        <form action="{{ route('verify') }}" class="mt-5" method="POST">
+            @csrf
+            @method('PUT')
+            <input type="hidden" value="{{ $id }}" name="esemenyId">
+            <input type="hidden" value="{{ Auth::user()['id'] }}" name="felhasznaloId">
             <button type="submit" class="rounded-xl text-gray-100 bg-green-600 text-2xl px-5 py-2.5 transition hover:bg-green-500">Megjelentem</button>
         </form>
+        @else
+            <p>Nem jelentkeztél büdös kölke!</p>
+        @endif
     </div>
 </div>
 
