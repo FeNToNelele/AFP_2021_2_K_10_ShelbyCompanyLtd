a5dc9ecb5e08a548e5f36bead16fea58c8a8a8e5
KecseKaroly <83023819+KecseKaroly@users.noreply.github.com>
2022-04-17 15:36:48 +0200

    Megerősítés route-ok

--- routes/web.php
+++ routes/web.php
@@ -36,4 +36,5 @@ Route::get('/events/{id}', [EventsController::class, 'listAnEvent'])->middleware
 Route::get('/createEvent', [EventsController::class, 'createEvent'])->name('createEvent')->middleware('auth');
 Route::post('/registerEvent', [EventsController::class, 'registerEvent'])->name('registerEvent')->middleware('auth');
 
-Route::get('/verify/{id}', [App\Http\Controllers\EventsController::class, 'verify'])->middleware('auth');
+Route::get('/verify/{id}', [App\Http\Controllers\EventsController::class, 'verify'])->middleware('auth')->name('verify');
+Route::put('/verify', [App\Http\Controllers\EventsController::class, 'verified'])->middleware('auth')->name('verify');
