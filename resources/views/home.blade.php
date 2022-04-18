28271a53628e220af3ddea110ce07e2cdaf7d276
FeNToNelele <sarosigab@gmail.com>
2022-04-18 12:08:30 +0200

    Értesítések menüpont design javítása

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,11 @@
 
 @section('content')
   @if (Session::has('message'))
-  <div class="mx-auto w-48 mt-3 py-2.5 rounded-xl bg-white shadow-xl">
+  <div class="mx-auto mt-3 py-2.5 rounded-xl bg-white shadow-xl">
     <div class="px-2 text-3xl text-black font-light text-center">{{ Session::get('message') }}</div>
   </div>
   @else
-  <div class="mx-auto w-48 mt-3 py-2.5 rounded-xl bg-white shadow-xl">
+  <div class="mx-auto mt-3 p-3 rounded-xl bg-white shadow-xl">
     <div class="px-2 text-3xl text-black font-light text-center">Sikeres bejelentkezés!</div>
   </div>
   @endif
