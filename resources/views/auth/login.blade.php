22ea78a1c52b990f231ee4861b1dc8abdbd920c8
Axel0103 <86410525+Axel0103@users.noreply.github.com>
2022-04-25 18:03:43 +0200

    Login front end szépítés

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,9 @@
                 <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Jelszó:</label>
                 <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required>
             </div>
+            <br><br>
             <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Bejelentkezés</button>
+            <br><br>
             <div class="text-sm font-light text-gray-900">
                 Nincs még fiókja? <a href="{{ route('register') }}" class="text-blue-700 hover:underline"><i>Regisztráljon</i></a>
             </div>
