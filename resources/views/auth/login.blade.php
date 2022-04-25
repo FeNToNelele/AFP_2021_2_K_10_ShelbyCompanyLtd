609a99f94308e45f206620b64363fc6e55eeb717
Axel0103 <86410525+Axel0103@users.noreply.github.com>
2022-04-25 18:01:09 +0200

    Elfelejtett jelszó funkció törlése

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,9 +16,6 @@
                 <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required>
             </div>
             <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Bejelentkezés</button>
-            <div class="flex items-start my-3">
-                <a href="{{ route('password.request') }}" class="text-sm font-light text-right text-blue-700 hover:underline"><i>Elfelejtette jelszavát?</i></a>
-            </div>
             <div class="text-sm font-light text-gray-900">
                 Nincs még fiókja? <a href="{{ route('register') }}" class="text-blue-700 hover:underline"><i>Regisztráljon</i></a>
             </div>
